0436994470afa01f3ba35d7bf2cb68d6c6c0fff6
Phillip Zierke <phillipzierke1@gmail.com>
2026-03-07 16:15:18 -0500

    Initial Commit - devsecops - Terraform directory and main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,33 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "eastus"
+
+  tags = {
+    environment = "Terraform Getting Started"
+    team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
+}
